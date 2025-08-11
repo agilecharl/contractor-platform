d98be43af42ab350ef6526b984d28c85f86a8ecc
agilecharl <agilecharl@gmail.com>
2025-08-11 13:14:27 +0200

    Initial commit

---
+++ apps/contractor-platform/app/app.tsx
@@ -0,0 +1,11 @@
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="@contractor-platform/contractor-platform" />
+    </div>
+  );
+}
+
+export default App;
